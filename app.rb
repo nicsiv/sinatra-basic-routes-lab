0785007bf419250958796bacc7ffbbe5c16f653b
nicsiv <nicoleusivek@gmail.com>
2020-11-16 00:49:57 -0600

    commit tree 3f7b71bc445a46721d3d11f16745559a8bb55078

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Nicole"
+    end
+
+    get '/hometown' do
+        "My hometown is Chicago"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Hey"
+    end
+
 end
